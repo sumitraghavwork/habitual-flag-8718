4da927b52bbc44bc91415ae2b3505fe8db108ece
sumitraghavwork <sumitraghavwork@gmail.com>
2022-11-09 10:56:47 +0530

    initial commit

---
+++ BookMyBus/src/main/java/com/bookmybus/BookMyBusApplication.java
@@ -0,0 +1,13 @@
+package com.bookmybus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyBusApplication.class, args);
+	}
+
+}
---
+++ BookMyBus/src/test/java/com/bookmybus/BookMyBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmybus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
