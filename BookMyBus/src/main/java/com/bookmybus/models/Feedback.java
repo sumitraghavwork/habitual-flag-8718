23b753018bd5cfcafbb2faba93846c34e4a6044b
VaibhavBedarkar <vaibhav.bedarkar98@gmail.com>
2022-11-09 22:58:31 +0530

    Added: Feedback bean and validation

--- BookMyBus/src/main/java/com/bookmybus/models/Feedback.java
+++ BookMyBus/src/main/java/com/bookmybus/models/Feedback.java
@@ -3,6 +3,13 @@ package com.bookmybus.models;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Repository;
 
@@ -18,12 +25,23 @@ import lombok.ToString;
 @Repository
 @Entity
 public class Feedback {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer feedbackId;
+	
+	@Min(1)
+	@Max(5)
 	private Integer driverRating;
+	@Min(1)
+	@Max(5)
 	private Integer serviceRating;
+	@Min(1)
+	@Max(5)
 	private Integer overallRating;
+	@NotNull(message = "Comment should not be null")
+	@NotBlank(message = "Comment should not be blank...!")
 	private String comments;
+	
 	private LocalDate feedbackdate;
 	private User user;
 	private Bus bus;
