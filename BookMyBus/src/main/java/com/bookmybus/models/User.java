fd9cfa2d45cfac30dfe1882a512fe4e8fd31ade9
sumitraghavwork <sumitraghavwork@gmail.com>
2022-11-13 10:01:58 +0530

    validations for admin and users

--- BookMyBus/src/main/java/com/bookmybus/models/User.java
+++ BookMyBus/src/main/java/com/bookmybus/models/User.java
@@ -29,24 +29,21 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer userLoginId;
 
-	@Size(min = 3, max = 20, message = "${user.invalid.userName}")
+	@Size(min = 3, max = 20, message = "{user.invalid.userName}")
 	@Column(unique = true)
 	private String userName;
 	
-	@Size(min = 8, max = 20, message = "${user.invalid.password}")
-//	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,30}$",message = "${user.invalid.password}")
+	
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",message = "{user.invalid.password}")
 	private String password;
 
-	@Size(min = 3, max = 20, message = "${user.invalid.firstName}")
-//	@Pattern(regexp = "^[a-z][A-Z]", message = "${user.invalid.firstName}")
+	@Pattern(regexp = "^[a-b][A-Z]{3,20}", message = "{user.invalid.firstName}")
 	private String firstName;
 
-	@Size(min = 3, max = 20, message = "${user.invalid.lastName}")
-//	@Pattern(regexp = "^[a-z][A-Z]", message = "${user.invalid.lastName}")
+	@Pattern(regexp = "^[a-b][A-Z]{3,20}", message = "{user.invalid.lastName}")
 	private String lastName;
 
-	@Size(min = 10, max = 10, message = "${user.invalid.contact}")
-//	@Pattern(regexp = "^[0-9]", message = "${user.invalid.contact}")
+	@Pattern(regexp = "^[0-9]{10}", message = "{user.invalid.contact}")
 	private String contact;
 
 	@Email(message = "incorrect email")
